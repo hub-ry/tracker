f2121af63a9bc78de5e0a24e81afcaffc461b094
hub-ry <rhubbart@purdue.edu>
2025-11-06 16:42:04 -0500

    outline

---
+++ weight.py
@@ -0,0 +1,36 @@
+def add_weight(date, weight):
+  with open('weightpy.txt', 'a') as file:
+    file.write(f'{date}, {weight}\n')
+  print('\nadded!\n')
+
+
+
+def main():
+  while (1):
+    print("1. Add a weight\n2. Modify the list\n3. View The List\n4. Data\n5. Exit")
+    choice = int(input(""))
+    if (choice == 1):
+      date = input("date:XX-XX-XX: ")
+      weight = input("weight:lbs: ")
+      add_weight(date, weight)
+      continue
+    if (choice == 2):
+      date = input("modifing date: ")
+      modify_type = int(input("1. Remove\n2.Change Date\n3.Change weight\n"))
+      # you can make one function with modify_type as parameter
+      continue
+    if (choice == 3):
+      print("hi")
+      #print the list
+      continue
+    if (choice == 4): 
+      print('hi')
+      #call fucntion that goes crazy and calculates stats
+      continue
+    if (choice == 5):
+      break
+    else:
+      print('invalid choice')
+if __name__ == "__main__":
+    main()
+ 
